8e68427c759e6288139e7da90d22c611734a83c7
frigo27 <frigo34@hotmail.com>
2021-07-04 12:48:03 -0300

    Alterado o terraform e o arquivo host

--- main.tf
+++ main.tf
@@ -23,6 +23,16 @@ resource "azurerm_subnet" "rg" {
   virtual_network_name = azurerm_virtual_network.rg.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+resource "azurerm_public_ip" "publicIp" {
+  name = "publicIP"  
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+    allocation_method   = "Static"
+
+  tags = {
+    environment = "teste azure"
+  }
+}
 
 resource "azurerm_network_interface" "rg" {
   name                = "rg-nic"
@@ -33,8 +43,14 @@ resource "azurerm_network_interface" "rg" {
     name                          = "intergal"
     subnet_id                     = azurerm_subnet.rg.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicIp.id
+  }
+    tags = {
+    environment = "teste azure"
   }
 }
+
+
 
 resource "azurerm_windows_virtual_machine" "rg" {
   name                = "rg-machine"
@@ -58,5 +74,89 @@ resource "azurerm_windows_virtual_machine" "rg" {
     sku       = "2019-Datacenter"
     version   = "latest"
   }
+}
+
+
+
+resource "azurerm_network_security_group" "allow_RDP_in" {
+  name                = "rg-machine"
+  location            = "eastus2"
+  resource_group_name         = azurerm_resource_group.rg.name
+
+
+  security_rule {
+    name                        = "RDP"
+    priority                    = 100
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "3389"
+    destination_port_range      = "3389"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+        
+  }
+  security_rule {
+    name                        = "RDP-out"
+    priority                    = 110
+    direction                   = "Outbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "3389"
+    destination_port_range      = "3389"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    
+  }
+  security_rule {
+    name                        = "WINRM-in"
+    priority                    = 120
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "5985"
+    destination_port_range      = "5985"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
+  security_rule {
+    name                        = "WINRM-out"
+    priority                    = 130
+    direction                   = "Outbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "5985"
+    destination_port_range      = "5985"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    
+  }
+
+  security_rule {
+    name                        = "WINRMs-in"
+    priority                    = 140
+    direction                   = "Inbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "5986"
+    destination_port_range      = "5986"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+    
+  }
+  security_rule {
+    name                        = "WINRMs-out"
+    priority                    = 150
+    direction                   = "Outbound"
+    access                      = "Allow"
+    protocol                    = "Tcp"
+    source_port_range           = "5986"
+    destination_port_range      = "5986"
+    source_address_prefix       = "*"
+    destination_address_prefix  = "*"
+  }
+}
+output "WindowServer2019_public_dns" {
+  value = "${azurerm_public_ip.publicIp}"
 }
 
